4fa508222bb390d45f138739e0a13635cdffb79c
jose@vauxoo.com <>
2013-10-17 21:41:37 -0430

    [ADD] Added script to execute partner_do_merge automatically in all partners

---
+++ do_merge.py
@@ -0,0 +1,71 @@
+#-*- coding:utf-8 -*-
+#
+import oerplib
+import psycopg2
+
+HOST=raw_input('Please insert the server host: ')
+DBHOST=raw_input('Please insert the data base host: ')
+PORT=raw_input('Please insert the server port: ')
+DBPORT=raw_input('Please insert the database port: ')
+DB=raw_input('Please insert the database name: ')
+USER=raw_input('Please insert the server user: ')
+PASS=raw_input('Please insert the password for server user: ')
+DBUSER=raw_input('Please insert the postgres user: ')
+DBPASS=raw_input('Please insert the password for postgres user: ')
+cur = False
+try:
+    conn = psycopg2.connect("dbname='%s' user='%s' host='%s' password='%s' port=%s" % (DB, DBUSER,
+                                                                                       DBHOST, 
+                                                                                       DBPASS,
+                                                                                       DBPORT))
+    cur = conn.cursor()
+except:
+    print "I am unable to connect to the database"
+
+con = oerplib.OERP(
+server=HOST, 
+database=DB, 
+port=PORT, 
+)  
+
+con.login(USER, PASS)
+
+def check_partner():
+    if len(ids.partner_ids) > 10:
+        ids.write({'partner_ids': [(6, 0, [i.id for i in ids.partner_ids[:5]])]})
+        cr.commit()
+    for partner in ids.partner_ids:
+        if partner.vat and partner.is_company:
+            ids.write({'dst_partner_id': partner.id})
+            cr.commit()
+            break
+
+    return True
+
+
+def do_merge():
+
+    merge_obj = con.get('base.partner.merge.automatic.wizard')
+#    base_id = merge_obj.create(cr, uid, {'group_by_email':True})              
+#    result = merge_obj.start_process_cb(cr, uid, [base_id])
+#    base_id = merge_obj.browse(cr, uid, result.get('res_id'))               
+#    state = base_id.state
+    if cur:
+        cur.execute("""SELECT min(id), array_agg(id)
+                       FROM res_partner 
+                       WHERE id NOT IN (SELECT id 
+                                        FROM res_partner 
+                                        WHERE vat ILIKE 'MXXAXX010101000' OR vat ILIKE '') 
+                       GROUP BY vat HAVING COUNT(*) >= 2
+                       ORDER BY min(id)""")
+
+        for min_id, aggr_ids in cur.fetchall():
+            partner_obj = con.get('res.partner')
+            if len(aggr_ids) < 10:
+                try:
+                    print aggr_ids
+                    merge_obj.merge_pbp(aggr_ids, False)
+                except:
+                    print 'Algo'
+    return True
+do_merge()
